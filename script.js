b07715467d443892740e1fe21eb2f66fcafc2281
Remo <81964236+remoremote@users.noreply.github.com>
2023-04-20 07:31:29 +0200

    small changes

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
 // ——————————*** MATCH CARD ITEM ***———————————
-// getting the Id's 
-const matchCard = document.getElementById("match-card");
-const playtimeValue = document.getElementById("playtime-value");
-const scoreValue = document.getElementById("score-value");
-const shootOnTargetLeftValue = document.getElementById("shoot-on-target-left-value");
-const shootOnTargetLeftBar = document.getElementById("shoot-on-target-left-bar");
-const shootOnTargetRightValue = document.getElementById("shoot-on-target-right-value");
-const shootOnTargetRightBar = document.getElementById("shoot-on-target-right-bar");
-const shootLeftValue = document.getElementById("shoot-left-value");
-const shootLeftBar = document.getElementById("shoot-left-bar");
-const shootRightValue = document.getElementById("shoot-right-value");
-const shootRightBar = document.getElementById("shoot-right-bar");
-const foulsLeftValue = document.getElementById("fouls-left-value");
-const foulsLeftBar = document.getElementById("fouls-left-bar");
-const foulsRightValue = document.getElementById("fouls-right-value");
-const foulsRightBar = document.getElementById("fouls-right-bar");
-
-// Set the dynamic content
-playtimeValue.textContent = "65 : 34";
-scoreValue.textContent = "3 - 100";
-shootOnTargetLeftValue.textContent = "5";
-shootOnTargetLeftBar.style.width = "50%";
-shootOnTargetRightValue.textContent = "2";
-shootOnTargetRightBar.style.width = "20%";
-shootLeftValue.textContent = "9";
-shootLeftBar.style.width = "75%";
-shootRightValue.textContent = "6";
-shootRightBar.style.width = "60%";
-foulsLeftValue.textContent = "5";
-foulsLeftBar.style.width = "50%";
-foulsRightValue.textContent = "4";
-foulsRightBar.style.width = "40%";
 
 
-// ——————————*** MATCH CARD ITEM ***———————————
+
+// ——————————*** TEAM OVERVIEW ITEM ***———————————
