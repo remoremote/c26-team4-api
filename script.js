29ca2b20018d84065652708b863b8a3bce367045
Remo <81964236+remoremote@users.noreply.github.com>
2023-04-21 14:05:22 +0200

    reset.css, grabing content with script and style

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 // ——————————*** MATCH CARD ITEM ***———————————
-
+// Dynamic data
+const matchData = {
+    playtime: '62 : 24',
+    score: '2 - 2',
+    stats: {
+      left: {
+        shotsOnTarget: 7,
+        shoot: 12,
+        fouls: 7,
+      },
+      right: {
+        shotsOnTarget: 3,
+        shoot: 7,
+        fouls: 3,
+      },
+    },
+  };
+  
+  // Update playtime and score
+  document.getElementById('playtime-value').textContent = matchData.playtime;
+  document.getElementById('score-value').textContent = matchData.score;
+  
+  // Update stats
+  document.getElementById('left-shots-on-target').textContent =
+    matchData.stats.left.shotsOnTarget;
+  document.getElementById('right-shots-on-target').textContent =
+    matchData.stats.right.shotsOnTarget;
+  
+  document.getElementById('left-shoot').textContent = matchData.stats.left.shoot;
+  document.getElementById('right-shoot').textContent = matchData.stats.right.shoot;
+  
+  document.getElementById('left-fouls').textContent = matchData.stats.left.fouls;
+  document.getElementById('right-fouls').textContent = matchData.stats.right.fouls;
+  
 
 // ——————————*** TEAM OVERVIEW ITEM ***———————————
