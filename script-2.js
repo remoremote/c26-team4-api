c0528196325cb74866b2adeacf8c871d53244dd8
Remo <81964236+remoremote@users.noreply.github.com>
2023-04-30 11:11:42 +0200

    script-3 because script-2 did not work lol

--- script-2.js
+++ script-2.js
@@ -1,175 +1,222 @@
 async function fetchStadiumData(stadiumId) {
     const apiKey = 'fa00ed0b5emsh0cfc2bed1d96245p19e0d1jsna00df65a08d3';
     const searchUrl = `https://api-football-v1.p.rapidapi.com/v3/venues?id=${stadiumId}`;
-  
+
     const response = await fetch(searchUrl, {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': apiKey
-      }
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey
+        }
     });
-  
+
     const data = await response.json();
     return data;
-  }
-  
-  async function fetchLeagueData(leagueId) {
+}
+
+async function fetchLeagueData(leagueId) {
     const apiKey = 'fa00ed0b5emsh0cfc2bed1d96245p19e0d1jsna00df65a08d3';
     const searchUrl = `https://api-football-v1.p.rapidapi.com/v3/leagues?id=${leagueId}`;
-  
+
     const response = await fetch(searchUrl, {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': apiKey
-      }
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey
+        }
     });
-  
+
     const data = await response.json();
     return data;
-  }
-  
-  async function fetchTeamStats(teamId, leagueId) {
+}
+
+async function fetchTeamStats(teamId, leagueId) {
     const apiKey = 'fa00ed0b5emsh0cfc2bed1d96245p19e0d1jsna00df65a08d3';
     const searchUrl = `https://api-football-v1.p.rapidapi.com/v3/teams/statistics?team=${teamId}&league=${leagueId}`;
-  
+
     const response = await fetch(searchUrl, {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': apiKey
-      }
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey
+        }
     });
-  
+
     const data = await response.json();
     return data.response;
-  }
+}
 
 // Get the input element
-const input = document.querySelector('.input-item input');
+const input = document.querySelector('#team-input');
+
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        updateData(event.target.value);
+    }
+});
 
 // Update data function
 async function updateData(teamName) {
-  const apiKey = 'fa00ed0b5emsh0cfc2bed1d96245p19e0d1jsna00df65a08d3';
-  const searchUrl = `https://api-football-v1.p.rapidapi.com/v3/teams?name=${teamName}`;
+    const apiKey = 'fa00ed0b5emsh0cfc2bed1d96245p19e0d1jsna00df65a08d3';
+    const searchUrl = `https://api-football-v1.p.rapidapi.com/v3/teams?name=${teamName}`;
 
-  const response = await fetch(searchUrl, {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': apiKey
-      }
-  });
+    const response = await fetch(searchUrl, {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': apiKey,
+        },
+    });
 
-  const data = await response.json();
-  const team = data.response[0].team;
+    const data = await response.json();
 
-  console.log('team:', team);
+    await handleTeamData(data);
+}
+
+async function handleTeamData(data) {
+    const team = data.response[0].team;
+
+    console.log('team:', team);
+
+    const leagueId = team.league ? team.league.id : null;
+
+    if (leagueId) {
+        // Use Promise.all to fetch data in parallel
+        const [stadiumData, leagueData, teamStats] = await Promise.all([
+            fetchStadiumData(team.venue_id),
+            fetchLeagueData(leagueId),
+            fetchTeamStats(team.id, leagueId)
+        ]);
+
+        updateUI(stadiumData, leagueData, teamStats);
+    } else {
+        updateUI(null, null, null);
+    }
+}
+
+// Fetch stadium, league, and match stats data and update the UI accordingly
+const stadiumData = await fetchStadiumData(team.venue_id);
+const stadium = (stadiumData.response.length > 0) ? stadiumData.response[0].venue : undefined;
+
+const leagueData = leagueId ? await fetchLeagueData(leagueId) : {
+    response: []
+}; // Fetch league data only if leagueId is not null
+const league = leagueData.response.length > 0 ? leagueData.response[0].league : undefined;
 
 
-  document.querySelector('.team-img img').src = team.logo;
-  document.querySelector('.team-facts h2').textContent = team.name;
-  document.querySelector('.team-facts p:nth-child(2)').textContent = team.founded;
-  document.querySelector('.team-facts p:nth-child(3)').textContent = team.country;
+const teamStats = team.league ? await fetchTeamStats(team.id, team.league.id) : null;
+const statistics = teamStats.response[0];
+console.log('teamStats:', teamStats);
+
+function updateUI(stadiumData, leagueData, teamStats) {
+    const stadium = (stadiumData && stadiumData.response.length > 0) ? stadiumData.response[0].venue : undefined;
+    const league = (leagueData && leagueData.response.length > 0) ? leagueData.response[0].league : undefined;
 
 
-  const leagueId = team.league ? team.league.id : null; // Check if team.league exists before accessing its properties
-
-      // Fetch stadium, league, and match stats data and update the UI accordingly
-      const stadiumData = await fetchStadiumData(team.venue_id);
-      const stadium = (stadiumData.response.length > 0) ? stadiumData.response[0].venue : undefined;
-    
-      const leagueData = leagueId ? await fetchLeagueData(leagueId) : { response: [] }; // Fetch league data only if leagueId is not null
-      const league = leagueData.response.length > 0 ? leagueData.response[0].league : undefined;
-    
-
-  const teamStats = await fetchTeamStats(team.id, team.league.id); 
-  const statistics = teamStats.response[0];
-  console.log('teamStats:', teamStats);
-
-
-    
     // Update stadium data only if 'stadium' is defined
-  if (stadium) {
-    document.querySelector('.stadion-details #stadion-city').textContent = stadium.city;
-    document.querySelector('.stadion-details #stadion-address').textContent = stadium.address;
-    document.querySelector('.stadion-details #stadion-capacity').textContent = stadium.capacity;
-    document.querySelector('.stadion-details #stadion-surface').textContent = stadium.surface;
-    document.querySelector('.stadion-img #stadion-name').textContent = stadium.name;
-    document.querySelector('.stadion-img img').src = stadium.image;
-  } else {
-    // Clear the stadium data in the UI if the 'stadium' is not available
-    document.querySelector('.stadion-details #stadion-city').textContent = '';
-    document.querySelector('.stadion-details #stadion-address').textContent = '';
-    document.querySelector('.stadion-details #stadion-capacity').textContent = '';
-    document.querySelector('.stadion-details #stadion-surface').textContent = '';
-    document.querySelector('.stadion-img #stadion-name').textContent = '';
-    document.querySelector('.stadion-img img').src = '';
-  }
-    
+    if (stadium) {
+        document.querySelector('.stadion-details #stadion-city').textContent = stadium.city;
+        document.querySelector('.stadion-details #stadion-address').textContent = stadium.address;
+        document.querySelector('.stadion-details #stadion-capacity').textContent = stadium.capacity;
+        document.querySelector('.stadion-details #stadion-surface').textContent = stadium.surface;
+        document.querySelector('.stadion-img #stadion-name').textContent = stadium.name;
+        document.querySelector('.stadion-img img').src = stadium.image;
+    } else {
+        // Clear the stadium data in the UI if the 'stadium' is not available
+        document.querySelector('.stadion-details #stadion-city').textContent = '';
+        document.querySelector('.stadion-details #stadion-address').textContent = '';
+        document.querySelector('.stadion-details #stadion-capacity').textContent = '';
+        document.querySelector('.stadion-details #stadion-surface').textContent = '';
+        document.querySelector('.stadion-img #stadion-name').textContent = '';
+        document.querySelector('.stadion-img img').src = '';
+    }
+
     // Update league data
     if (league) {
         document.querySelector('.league-info #league-name').textContent = league.name;
         document.querySelector('.league-info #league-year').textContent = league.season;
         document.querySelector('.league-info #league-country').textContent = league.country;
-      } else {
+    } else {
         // Clear the league data in the UI if the 'league' is not available
         document.querySelector('.league-info #league-name').textContent = '';
         document.querySelector('.league-info #league-year').textContent = '';
         document.querySelector('.league-info #league-country').textContent = '';
-      }
-    
-// Update match stats
-document.getElementById('nr-of-matches').textContent = teamStats.statistics.played.total;
-document.getElementById('matches-home').textContent = teamStats.statistics.played.home;
-document.getElementById('matches-away').textContent = teamStats.statistics.played.away;
-
-// Update goals
-document.querySelector('.card-goals #nr-of-goals').textContent = teamStats.statistics.goals.total;
-document.querySelector('.card-goals #goals-home').textContent = teamStats.statistics.goals.home;
-document.querySelector('.card-goals #goals-away').textContent = teamStats.statistics.goals.away;
-
-// Update average goals
-document.getElementById('avg-nr-of-goals').textContent = (teamStats.statistics.goals.total / teamStats.statistics.played.total).toFixed(1);
-document.getElementById('avg-goals-home').textContent = (teamStats.statistics.goals.home / teamStats.statistics.played.home).toFixed(1);
-document.getElementById('avg-goals-away').textContent = (teamStats.statistics.goals.away / teamStats.statistics.played.away).toFixed(1);
-
-// Update wins
-document.getElementById('nr-of-wins').textContent = teamStats.statistics.wins.total;
-document.getElementById('wins-home').textContent = teamStats.statistics.wins.home;
-document.getElementById('wins-away').textContent = teamStats.statistics.wins.away;
-
-// Update draws
-document.getElementById('nr-of-draws').textContent = teamStats.statistics.draws.total;
-document.getElementById('draws-home').textContent = teamStats.statistics.draws.home;
-document.getElementById('draws-away').textContent = teamStats.statistics.draws.away;
-
-// Update losses
-document.getElementById('nr-of-losses').textContent = teamStats.statistics.loses.total;
-document.getElementById('losses-home').textContent = teamStats.statistics.loses.home;
-document.getElementById('losses-away').textContent = teamStats.statistics.loses.away;
+    }
 
 
 
-  }
-  
-  let timeout;
+    if (teamStats) {
+        const statistics = teamStats.response[0];
+        // Update match stats
+        document.querySelector('#nr-of-matches').textContent = teamStats.played.total;
+        document.querySelector('#matches-home').textContent = teamStats.played.home;
+        document.querySelector('#matches-away').textContent = teamStats.played.away;
+
+        // Update goals
+        document.querySelector('.card-goals #nr-of-goals').textContent = teamStats.goals.total;
+        document.querySelector('.card-goals #goals-home').textContent = teamStats.goals.home;
+        document.querySelector('.card-goals #goals-away').textContent = teamStats.goals.away;
+
+        // Update average goals
+        document.getElementById('avg-nr-of-goals').textContent = (teamStats.goals.total / teamStats.played.total).toFixed(1);
+        document.getElementById('avg-goals-home').textContent = (teamStats.goals.home / teamStats.played.home).toFixed(1);
+        document.getElementById('avg-goals-away').textContent = (teamStats.goals.away / teamStats.played.away).toFixed(1);
+
+        // Update wins
+        document.getElementById('nr-of-wins').textContent = teamStats.wins.total;
+        document.getElementById('wins-home').textContent = teamStats.wins.home;
+        document.getElementById('wins-away').textContent = teamStats.wins.away;
+
+        // Update draws
+        document.getElementById('nr-of-draws').textContent = teamStats.draws.total;
+        document.getElementById('draws-home').textContent = teamStats.draws.home;
+        document.getElementById('draws-away').textContent = teamStats.draws.away;
+
+        // Update losses
+        document.getElementById('nr-of-losses').textContent = teamStats.loses.total;
+        document.getElementById('losses-home').textContent = teamStats.loses.home;
+        document.getElementById('losses-away').textContent = teamStats.loses.away;
 
 
-  
-  const searchButton = document.getElementById('search-button');
+    } else {
+        // Clear the data in the UI if 'teamStats' is not available
+        const elementsToUpdate = [
+            '#nr-of-matches', '#matches-home', '#matches-away',
+            '#nr-of-goals', '#goals-home', '#goals-away',
+            '#avg-nr-of-goals', '#avg-goals-home', '#avg-goals-away',
+            '#nr-of-wins', '#wins-home', '#wins-away',
+            '#nr-of-draws', '#draws-home', '#draws-away',
+            '#nr-of-losses', '#losses-home', '#losses-away'
+        ];
 
-  searchButton.addEventListener('click', () => {
+        elementsToUpdate.forEach(selector => {
+            document.querySelector(selector).textContent = '';
+        });
+    }
+
+    console.log('stadiumData:', stadiumData);
+    console.log('leagueData:', leagueData);
+    console.log('teamStats:', teamStats);
+
+
+}
+
+let timeout;
+
+
+
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', () => {
     const teamName = input.value.trim(); // Remove leading/trailing whitespace characters
     if (teamName.length > 0) {
-      updateData(teamName);
+        updateData(teamName);
     } else {
-      console.log('Please enter a valid team name');
+        console.log('Please enter a valid team name');
     }
-  });
-  
+});
+
 
 // Set a placeholder image when the image fails to load
 document.querySelector('.team-img img').addEventListener('error', (e) => {
     e.target.src = 'https://www.ncenet.com/wp-content/uploads/2020/04/No-image-found.jpg';
-  });
-  document.querySelector('.stadion-img img').addEventListener('error', (e) => {
+});
+document.querySelector('.stadion-img img').addEventListener('error', (e) => {
     e.target.src = 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png';
-  });
+});
---
+++ script-3.js
@@ -0,0 +1,115 @@
+async function fetchLeagueAndStats(teamId, apiKey) {
+    const teamStatsUrl = `https://v3.football.api-sports.io/teams/${teamId}/statistics`;
+    const teamStatsResponse = await fetch(teamStatsUrl, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
+        'x-rapidapi-key': apiKey
+      }
+    });
+    const teamStatsData = await teamStatsResponse.json();
+    const stats = teamStatsData.data;
+  
+    const leagueId = stats.league.id;
+    const leagueUrl = `https://v3.football.api-sports.io/leagues/${leagueId}`;
+    const leagueResponse = await fetch(leagueUrl, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
+        'x-rapidapi-key': apiKey
+      }
+    });
+    const leagueData = await leagueResponse.json();
+    const league = leagueData.data;
+  
+    document.getElementById('league-name').innerText = league.name;
+    document.getElementById('league-year').innerText = league.season;
+    document.getElementById('league-country').innerText = league.country;
+  
+    document.getElementById('nr-of-matches').innerText = stats.fixtures.played.total;
+    document.getElementById('matches-home').innerText = stats.fixtures.played.home;
+    document.getElementById('matches-away').innerText = stats.fixtures.played.away;
+  
+    document.getElementById('nr-of-goals').innerText = stats.goals.total;
+    document.getElementById('goals-home').innerText = stats.goals.home;
+    document.getElementById('goals-away').innerText = stats.goals.away;
+  
+    document.getElementById('avg-nr-of-goals').innerText = (stats.goals.total / stats.fixtures.played.total).toFixed(1);
+    document.getElementById('avg-goals-home').innerText = (stats.goals.home / stats.fixtures.played.home).toFixed(1);
+    document.getElementById('avg-goals-away').innerText = (stats.goals.away / stats.fixtures.played.away).toFixed(1);
+  
+    document.getElementById('nr-of-wins').innerText = stats.fixtures.wins.total;
+    document.getElementById('wins-home').innerText = stats.fixtures.wins.home;
+    document.getElementById('wins-away').innerText = stats.fixtures.wins.away;
+  
+    document.getElementById('nr-of-draws').innerText = stats.fixtures.draws.total;
+    document.getElementById('draws-home').innerText = stats.fixtures.draws.home;
+    document.getElementById('draws-away').innerText = stats.fixtures.draws.away;
+  
+    document.getElementById('nr-of-losses').innerText = stats.fixtures.loses.total;
+    document.getElementById('losses-home').innerText = stats.fixtures.loses.home;
+    document.getElementById('losses-away').innerText = stats.fixtures.loses.away;
+  }
+  
+  document.getElementById('search-button').addEventListener('click', async () => {
+    console.log("Button clicked"); 
+    const teamName = document.getElementById('team-input').value;
+    const apiKey = 'fa00ed0b5emsh0cfc2bed1d96245p19e0d1jsna00df65a08d3';
+  
+    if (teamName) {
+      const searchUrl = `https://v3.football.api-sports.io/teams?search=${teamName}`;
+      const teamResponse = await fetch(searchUrl, {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'v3.football.api-sports.io',
+          'x-rapidapi-key': apiKey
+        }
+      });
+  
+      const teamData = await teamResponse.json();
+  
+      if (teamData && teamData.data.length > 0) {
+        const team = teamData.data[0];
+        const venueUrl = `https://v3.football.api-sports.io/venues?id=${team.venue.id}`;
+        const venueResponse = await fetch(venueUrl, {
+          method: 'GET',
+          headers: {
+            'x-rapidapi-host': 'v3.football.api-sports.io',
+            'x-rapidapi-key': apiKey
+          }
+        });
+  
+        const venueData = await venueResponse.json();
+        const venue = venueData.data[0];
+  
+        // Update HTML elements with fetched data
+        document.querySelector('.team-img img').src = team.logo;
+        document.querySelector('.team-facts h2').innerText = team.name;
+        document.querySelector('.team-facts p:nth-child(2)').innerText = team.founded;
+        document.querySelector('.team-facts p:nth-child(3)').innerText = team.country;
+  
+        document.getElementById('stadion-city').innerText = venue.city;
+        document.getElementById('stadion-address').innerText = venue.address;
+        document.getElementById('stadion-capacity').innerText = venue.capacity;
+        document.getElementById('stadion-surface').innerText = venue.surface;
+  
+        // Fetch and update league and stats data
+        await fetchLeagueAndStats(team.id, apiKey);
+      }
+    }
+  });
+  
+
+  fetch(searchUrl, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'v3.football.api-sports.io',
+      'x-rapidapi-key': apiKey
+    }
+  })
+  .then(response => response.json())
+  .then(teamData => {
+    // Rest of your teamData handling code
+  })
+  .catch(error => console.error("Error fetching team data:", error));
+  
