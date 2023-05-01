9be7c40a0e7e03b56bde6547b6bde240ba96196f
Remo <81964236+remoremote@users.noreply.github.com>
2023-05-01 15:53:27 +0200

    updating script

--- script-4.js
+++ script-4.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return await response.json();
     }
 
-    async function getTeamIdByName(apiKey, teamName, teamCountry) {
+    async function getTeamIdByName(apiKey, teamName) {
         const searchUrl = `${apiUrl}/teams?search=${teamName}`;
         const options = {
             method: 'GET',
@@ -24,30 +24,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const searchData = await fetchData(searchUrl, options);
         console.log('API response:', searchData); // Log the API response
         if (searchData.results > 0) {
-            const filteredResults = searchData.response.filter(team => team.country === teamCountry);
-            if (filteredResults.length > 0) {
-                return filteredResults[0].id;
-            } else {
-                return null;
+            // Iterate over the results and return the team ID if the name matches
+            for (let team of searchData.response) {
+                console.log('Team:', team); // Log the team object
+                console.log('Teams found:', searchData.response); //see all the teams found in the response
+                if (team.team.name.toLowerCase() === teamName.toLowerCase()) {
+                    return team.team.id;
+                }
             }
-        } else {
-            return null;
         }
+        return null;
     }
-
-
-    async function displayTeamInfoByName(apiKey, teamName, teamCountry) {
-        const teamId = await getTeamIdByName(apiKey, teamName, teamCountry);
-        if (teamId) {
-            console.log('Team ID:', teamId); // Log the team ID to ensure it's being retrieved correctly
-            await fetchAndDisplayTeamInfo(apiKey, teamId);
-        } else {
-            console.error('Team not found');
-        }
+    
+    function displayNotFoundMessage() {
+        const notFoundMessage = "Team not found";
+        document.querySelector('.team-facts h2').innerText = notFoundMessage;
+        // Add any other elements you want to clear or update when the team is not found
     }
-
-
-    async function fetchAndDisplayTeamInfo(apiKey, teamId) {
+    
+    async function fetchAndDisplayTeamStats(apiKey, teamId, season) {
         const options = {
             method: 'GET',
             headers: {
@@ -55,79 +50,119 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
             }
         };
-
+    
         const teamUrl = `${apiUrl}/teams?id=${teamId}`;
         const teamData = await fetchData(teamUrl, options);
         const {
-            team,
-            venue
+            team: {
+                team,
+                venue
+            }
         } = teamData.response[0];
-
+    
         const leagueUrl = `${apiUrl}/leagues?team=${teamId}`;
         const leagueData = await fetchData(leagueUrl, options);
         const league = leagueData.response[0].league;
-
-        const statsUrl = `${apiUrl}/teams/statistics?league=${league.id}&season=${league.season}&team=${teamId}`;
+    
+        const statsUrl = `${apiUrl}/teams/statistics?league=${league.id}&season=${season}&team=${teamId}`;
         const statsData = await fetchData(statsUrl, options);
         const stats = statsData.response;
-
-        updateHTML(stats, league, team, venue);
+    
+        updateHTML(stats[0], league, team, venue); // Update stats, instead of the whole stats object
+    
+        console.log("stats:", stats);
+        console.log("league:", league);
+        console.log("team:", team);
+        console.log("venue:", venue);
+    
+    }    
+    
+    async function fetchAndDisplayTeamInfo(apiKey, teamName, season) {
+        const teamId = await getTeamIdByName(apiKey, teamName);
+        if (teamId) {
+            console.log('Team ID:', teamId); // Log the team ID to ensure it's being retrieved correctly
+            await fetchAndDisplayTeamStats(apiKey, teamId, season); // call the renamed function instead of itself
+        } else {
+            displayNotFoundMessage();
+        }
     }
-
+    
     function updateHTML(stats, league, team, venue) {
+        if (!stats || !league || !team || !venue) return; // return if any of the data is missing
+    
         // Update team information
+        document.getElementById('team-input').value = team.name;
+        document.getElementById('search-button').innerText = 'Search';
+    
+        // Update team details
         document.querySelector('.team-img img').src = team.logo;
         document.querySelector('.team-facts h2').innerText = team.name;
         document.querySelector('.team-facts p:nth-child(2)').innerText = team.founded;
         document.querySelector('.team-facts p:nth-child(3)').innerText = team.country;
-
+    
         // Update venue information
         document.getElementById('stadion-city').innerText = venue.city;
         document.getElementById('stadion-address').innerText = venue.address;
         document.getElementById('stadion-capacity').innerText = venue.capacity;
         document.getElementById('stadion-surface').innerText = venue.surface;
         document.getElementById('stadion-name').innerText = venue.name;
-        document.getElementById('stadion-img').src = venue.photo;
-
+        document.getElementById('stadion-img').src = venue.image;
+    
         // Update league information
         document.getElementById('league-name').innerText = league.name;
-        document.getElementById('league-year').innerText = league.season;
+        document.getElementById('league-year').innerText = stats[0].fixture.season.year;
         document.getElementById('league-country').innerText = league.country;
+    
+        // Update match statistics
+        document.getElementById('nr-of-matches').innerText = stats[0].league.fixtures.played.total;
+        document.getElementById('matches-home').innerText = stats[0].league.fixtures.played.home;
+        document.getElementById('matches-away').innerText = stats[0].league.fixtures.played.away;
+    
+        // Update goal statistics
+        document.getElementById('nr-of-goals').innerText = stats[0].goals.for.total;
+        document.getElementById('goals-home').innerText = stats[0].goals.for.home;
+        document.getElementById('goals-away').innerText = stats[0].goals.for.away;
+    
+        // Update average goal statistics
+        document.getElementById('avg-nr-of-goals').innerText = stats[0].goals.for.average.total;
+        document.getElementById('avg-goals-home').innerText = stats[0].goals.for.average.home;
+        document.getElementById('avg-goals-away').innerText = stats[0].goals.for.average.away;
+    
+        // Update win statistics
+        document.getElementById('nr-of-wins').innerText = stats[0].league.wins.total;
+        document.getElementById('wins-home').innerText = stats[0].league.wins.home;
+        document.getElementById('wins-away').innerText = stats[0].league.wins.away;
+    
+        // Update loss statistics
+        document.getElementById('nr-of-losses').innerText = stats.loses.total;
+        document.getElementById('losses-home').innerText = stats.loses.home;
+        document.getElementById('losses-away').innerText = stats.loses.away;
 
-        // Update team stats
-        document.getElementById('nr-of-matches').innerText = stats.fixtures.played.total;
-        document.getElementById('matches-home').innerText = stats.fixtures.played.home;
-        document.getElementById('matches-away').innerText = stats.fixtures.played.away;
-        document.getElementById('nr-of-goals').innerText = stats.goals.total;
-        document.getElementById('goals-home').innerText = stats.goals.home;
-        document.getElementById('goals-away').innerText = stats.goals.away;
-        document.getElementById('avg-nr-of-goals').innerText = (stats.goals.total / stats.fixtures.played.total).toFixed(1);
-        document.getElementById('avg-goals-home').innerText = (stats.goals.home / stats.fixtures.played.home).toFixed(1);
-        document.getElementById('avg-goals-away').innerText = (stats.goals.away / stats.fixtures.played.away).toFixed(1);
-        document.getElementById('nr-of-wins').innerText = stats.fixtures.wins.total;
-        document.getElementById('wins-home').innerText = stats.fixtures.wins.home;
-        document.getElementById('wins-away').innerText = stats.fixtures.wins.away;
-        document.getElementById('nr-of-draws').innerText = stats.fixtures.draws.total;
-        document.getElementById('draws-home').innerText = stats.fixtures.draws.home;
-        document.getElementById('draws-away').innerText = stats.fixtures.draws.away;
-        document.getElementById('nr-of-losses').innerText = stats.fixtures.loses.total;
-        document.getElementById('losses-home').innerText = stats.fixtures.loses.home;
-        document.getElementById('losses-away').innerText = stats.fixtures.loses.away;
+        // Update clean sheet statistics
+        document.getElementById('clean-sheet-home').innerText = stats.clean_sheet.home;
+        document.getElementById('clean-sheet-away').innerText = stats.clean_sheet.away;
+        document.getElementById('clean-sheet-total').innerText = stats.clean_sheet.total;
+
+        // Update failed to score statistics
+        document.getElementById('failed-to-score-home').innerText = stats.failed_to_score.home;
+        document.getElementById('failed-to-score-away').innerText = stats.failed_to_score.away;
+        document.getElementById('failed-to-score-total').innerText = stats.failed_to_score.total;
+    
+        // Update failed to score statistics
+        document.getElementById('failed-to-score-home').innerText = failed_to_score.home;
+        document.getElementById('failed-to-score-away').innerText = failed_to_score.away;
+        document.getElementById('failed-to-score-total').innerText = failed_to_score.total;
     }
-
-    // Event listener for search button
+    
     const searchButton = document.getElementById('search-button');
-    const teamInput = document.getElementById('team-input');
-
+    
     searchButton.addEventListener('click', () => {
-        const teamName = teamInput.value;
-        const teamCountry = "England"; // Replace with the desired country for the team
-        displayTeamInfoByName(apiKey, teamName, teamCountry);
+        const teamName = document.getElementById('team-input').value.trim();
+        if (teamName) {
+            fetchAndDisplayTeamInfo(apiKey, teamName, '2021');
+            searchButton.innerText = 'Searching...';
+        }
     });
-
-
-    const defaultTeamName = "Arsenal";
-    const defaultTeamCountry = "England";
-    displayTeamInfoByName(apiKey, defaultTeamName, defaultTeamCountry);
-
+    
 });
+
